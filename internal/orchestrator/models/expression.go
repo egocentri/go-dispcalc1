af35528181e38d06542537e4c913d56927c88346
egocentri <d.kravchenko@naash.ru>
2025-02-24 13:54:55 +0300

    Delete internal/orchestrator/models directory

--- internal/orchestrator/models/expression.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Expression struct {
-	ID         int     `json:"id"`
-	Expression string  `json:"expression"`
-	Status     string  `json:"status"` 
-	Result     float64 `json:"result,omitempty"`
-}
