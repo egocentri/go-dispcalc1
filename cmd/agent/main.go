6c39e7f79ccfbc62e86183987feb23de478396a7
egocentri <d.kravchenko@naash.ru>
2025-02-24 13:41:34 +0300

    Create main.go

---
+++ cmd/agent/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "log"
+    "os"
+    "strconv"
+    "time"
+
+    "github.com/egocentri/go-dispcalc1/internal/agent"
+    "github.com/egocentri/go-dispcalc1/internal/config"
+)
+
+func main() {
+    cfg := config.InitEnv()
+    computingPower := 2
+    if val := os.Getenv("COMPUTING_POWER"); val != "" {
+        if cp, err := strconv.Atoi(val); err == nil {
+            computingPower = cp
+        }
+    }
+    orchestratorURL := "http://localhost:8080"
+    if val := os.Getenv("ORCHESTRATOR_URL"); val != "" {
+        orchestratorURL = val
+    }
+
+    log.Printf("Agent started. Orchestrator URL: %s, computingPower: %d\n", orchestratorURL, computingPower)
+    worker := agent.NewWorker(orchestratorURL, computingPower, cfg)
+    for {
+        worker.RequestAndProcessTask()
+        time.Sleep(time.Second) 
+    }
+}
