380480ff94ead2e45f2f2ec419b38ba2d0db2239
egocentri <d.kravchenko@naash.ru>
2025-02-25 21:03:25 +0300

    Update main.go

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,32 +1,33 @@
 package main
 
 import (
-    "log"
-    "os"
-    "strconv"
-    "time"
+	"log"
+	"os"
+	"strconv"
+	"time"
 
-    "github.com/egocentri/go-dispcalc1/internal/agent"
-    "github.com/egocentri/go-dispcalc1/internal/config"
+	"github.com/egocentri/go-dispcalc1/internal/agent"
+	"github.com/egocentri/go-dispcalc1/internal/config"
 )
 
 func main() {
-    cfg := config.InitEnv()
-    computingPower := 2
-    if val := os.Getenv("COMPUTING_POWER"); val != "" {
-        if cp, err := strconv.Atoi(val); err == nil {
-            computingPower = cp
-        }
-    }
-    orchestratorURL := "http://localhost:8080"
-    if val := os.Getenv("ORCHESTRATOR_URL"); val != "" {
-        orchestratorURL = val
-    }
+	cfg := config.InitEnv()
+	computingPower := 2
+	if val := os.Getenv("COMPUTING_POWER"); val != "" {
+		if cp, err := strconv.Atoi(val); err == nil {
+			computingPower = cp
+		}
+	}
 
-    log.Printf("Agent started. Orchestrator URL: %s, computingPower: %d\n", orchestratorURL, computingPower)
-    worker := agent.NewWorker(orchestratorURL, computingPower, cfg)
-    for {
-        worker.RequestAndProcessTask()
-        time.Sleep(time.Second) 
-    }
+	// URL оркестратора
+	orchestratorURL := "http://localhost:8080"
+	if val := os.Getenv("ORCHESTRATOR_URL"); val != "" {
+		orchestratorURL = val
+	}
+	log.Printf("Agent started. Orchestrator URL: %s, computingPower: %d\n", orchestratorURL, computingPower)
+	worker := agent.NewWorker(orchestratorURL, computingPower, cfg)
+	for {
+		worker.RequestAndProcessTask()
+		time.Sleep(time.Second)
+	}
 }
