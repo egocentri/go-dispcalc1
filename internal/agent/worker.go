fee81ba2e62b2655c8b94aafe7afec51df71656e
egocentri <d.kravchenko@naash.ru>
2025-02-25 21:12:22 +0300

    Update worker.go

--- internal/agent/worker.go
+++ internal/agent/worker.go
@@ -1,54 +1,51 @@
 package agent
 
 import (
-    "log"
-    "time"
+	"log"
+	"time"
 
-    "github.com/egocentri/go-dispcalc1/internal/agent/client"
-    "github.com/egocentri/go-dispcalc1/internal/config"
-    "github.com/egocentri/go-dispcalc1/internal/models"
-    "github.com/egocentri/go-dispcalc1/internal/orchestrator/services"
+	"github.com/egocentri/go-dispcalc1/internal/agent/client"
+	"github.com/egocentri/go-dispcalc1/internal/config"
+	"github.com/egocentri/go-dispcalc1/internal/orchestrator/services"
+	"github.com/egocentri/go-dispcalc1/internal/models"
 )
 
 type Worker struct {
-    orchestratorURL string
-    computingPower  int
-    cfg             *config.EnvConfig
-    client          *client.AgentClient
+	orchestratorURL string
+	computingPower  int
+	cfg             *config.EnvConfig
+	client          *client.AgentClient
 }
 
 func NewWorker(url string, cp int, cfg *config.EnvConfig) *Worker {
-    return &Worker{
-        orchestratorURL: url,
-        computingPower:  cp,
-        cfg:             cfg,
-        client:          client.NewAgentClient(url),
-    }
+	return &Worker{
+		orchestratorURL: url,
+		computingPower:  cp,
+		cfg:             cfg,
+		client:          client.NewAgentClient(url),
+	}
 }
 
 func (w *Worker) RequestAndProcessTask() {
-    task, err := w.client.GetTask()
-    if err != nil {
-        log.Println("No tasks or error:", err)
-        return
-    }
-    log.Printf("Received task ID=%d, expr=%s\n", task.ID, task.Arg1)
-    time.Sleep(time.Duration(task.OperationTime) * time.Millisecond)
+	task, err := w.client.GetTask()
+	if err != nil {
+		log.Println("No tasks or error:", err)
+		return
+	}
 
-    // Вычисляем (вызов вашего парсера/ EvaluateExpression)
-    result, err := services.EvaluateExpression(task.Arg1)
-    if err != nil {
-        log.Printf("Error evaluating expression: %v\n", err)
-        // Можно отправить ошибку, но ТЗ этого не оговаривает
-        return
-    }
-
-    if err := w.client.PostTaskResult(models.TaskResultRequest{
-        ID:     task.ID,
-        Result: result,
-    }); err != nil {
-        log.Printf("Error posting task result: %v\n", err)
-        return
-    }
-    log.Printf("Task ID=%d completed, result=%.2f\n", task.ID, result)
+	log.Printf("Received task ID=%d, expr=%s\n", task.ID, task.Arg1)
+	time.Sleep(time.Duration(task.OperationTime) * time.Millisecond)
+	result, err := services.EvaluateExpression(task.Arg1)
+	if err != nil {
+		log.Printf("Error evaluating expression: %v\n", err)
+		return
+	}
+	if err := w.client.PostTaskResult(models.TaskResultRequest{
+		ID:     task.ID,
+		Result: result,
+	}); err != nil {
+		log.Printf("Error posting task result: %v\n", err)
+		return
+	}
+	log.Printf("Task ID=%d completed, result=%.2f\n", task.ID, result)
 }
