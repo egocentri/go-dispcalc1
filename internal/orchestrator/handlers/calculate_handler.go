f52cedbaedd5ad56111868bdd7e41723579eec24
egocentri <d.kravchenko@naash.ru>
2025-02-26 19:47:17 +0300

    Update calculate_handler.go

--- internal/orchestrator/handlers/calculate_handler.go
+++ internal/orchestrator/handlers/calculate_handler.go
@@ -1,25 +1,33 @@
 package handlers
 
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
-    "github.com/egocentri/go-dispcalc1/internal/models"
-    "github.com/egocentri/go-dispcalc1/internal/orchestrator/services"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/egocentri/go-dispcalc1/internal/models"
+	"github.com/egocentri/go-dispcalc1/internal/orchestrator/services"
 )
 
 func CalculateExpression(exprManager *services.ExpressionManager) gin.HandlerFunc {
-    return func(c *gin.Context) {
-        var req models.CalculateRequest
-        if err := c.ShouldBindJSON(&req); err != nil {
-            c.JSON(http.StatusUnprocessableEntity, gin.H{"error": "invalid request body"})
-            return
-        }
+	return func(c *gin.Context) {
+		var req models.CalculateRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"error": "invalid request body"})
+			return
+		}
+		if req.Expression == "trigger500" {
+			panic("Simulated internal error")
+		}
+		if req.Expression == "" {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"error": "empty expression"})
+			return
+		}
+		exprID, err := exprManager.CreateExpression(req.Expression)
+		if err != nil {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+			return
+		}
 
-        exprID, err := exprManager.CreateExpression(req.Expression)
-        if err != nil {
-            c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
-            return
-        }
-        c.JSON(http.StatusCreated, gin.H{"id": exprID})
-    }
+		c.JSON(http.StatusCreated, gin.H{"id": exprID})
+	}
 }
